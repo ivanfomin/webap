f8e7327eb21c82064c8e699972b8070d858ddf07
ivanfomin <la-verne@mail.ru>
2015-04-24 13:21:33 +0300

    first commit

---
+++ lesson01/New_Class.java
@@ -0,0 +1,18 @@
+package lesson01;
+
+/**
+ * Created by ioan on 27.02.15.
+ */
+public class New_Class {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+        System.out.println("dfhgtfggft");
+        System.out.println("dfhgtfggft");
+        System.out.println("dfhgtfggft");
+        System.out.println("dfhgtfggft");
+        System.out.println("dfhgtfggft");
+        int i = 123;
+
+
+    }
+}
